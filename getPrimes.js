65b32d969b70f9192c4c9020701665c012f88c57
Osaro Iyoha <saro.iyoha@gmail.com>
2017-05-08 22:22:56 +0100

    Fixed '3' not Displaying issue

--- getPrimes.js
+++ getPrimes.js
@@ -10,6 +10,8 @@ let isPrime = (N)=> // This fuction confirms if numbers within a given range are
 }
 let getPrimes = (N)=> {// this fuction prints prime numbers from 0 to N into an array. Works(tested) for the range 0<=N<=1000000 .Execution time varies fron 2secs(Values from 5000 and below) to 18secs depending on input
   let primeArray = [];  
+  primeArray.push(2);// 2 and 3 are prime numbers
+  primeArray.push(3);
    for (let i = 2; i != N; ++i)
      if (isPrime(i))
      {
