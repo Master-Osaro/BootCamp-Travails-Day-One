89cde59fa7ed326df2e1c45d79f4878375572c0b
Osaro Iyoha <saro.iyoha@gmail.com>
2017-05-09 17:45:09 +0100

    Add files via upload

---
+++ OOP/app/arithgeo.js
@@ -0,0 +1,43 @@
+
+'use strict'
+
+
+
+module.exports = {	
+
+	aritGeo: function(arr) { 
+
+var diff = arr[1] - arr[0];
+var ratio = arr[1] / arr[0];
+
+
+var arith = true;
+var geo = true;
+
+for(var i = 0; i < arr.length - 1; i++)
+{
+    if( arr[i + 1] - arr[i] !== diff )
+      arith = false;
+    if(arr[i + 1] / ratio !== arr[i])
+      geo = false;
+    
+}
+ if (arr.length === 0) {
+
+   	  return 0;
+
+   	}
+
+
+if(arith === true)
+    return "Arithmetic";
+
+else if(geo === true)
+    return "Geometric";
+else
+    return -1;
+
+
+}
+
+}
