f1b0bce38557561b514766c57f7b65d8fef4915b
Osaro Iyoha <saro.iyoha@gmail.com>
2017-05-09 16:04:00 +0100

    Fixed getPrime hard-coding issue
    Thanks Koya!

--- getPrimes.js
+++ getPrimes.js
@@ -10,13 +10,25 @@ let isPrime = (N)=> // This fuction confirms if numbers within a given range are
 }
 let getPrimes = (N)=> {
   let primeArray = [];  
-  primeArray.push(2);// 2 and 3 are prime numbers
-  primeArray.push(3);
-   for (let i = 2; i != N; ++i)
+  if(N === 2){
+    primeArray.push(N);
+    return primeArray;
+  }
+  else if(N === 3){
+    primeArray.push(N-1);// 2 and 3 are prime numbers
+    primeArray.push(N);
+    return primeArray;
+  }
+  
+  else{
+    primeArray.push(2);
+    primeArray.push(3);
+   for (let i = 2; i <= N; ++i)
      if (isPrime(i))
      {
        primeArray.push(i);
      }
   return primeArray;
 }
-getPrimes(5000);
+}
+getPrimes(4);
